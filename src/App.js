750d6a0e454a9e33ed7d7887787c3ac6e983e172
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-08-13 19:35:33 +0330

    write login and create components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css";
 import {BrowserRouter as Router , Routes , Route, Link} from "react-router-dom"
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
+import {signOut} from "firebase/auth"
+import { auth } from './firebase-config';
+
 
 export default function App() {
+  const [isAuth,setIsAuth] = useState(false)
+
+
+  const signUserOut = ()=>{
+    signOut(auth).then(()=>{
+      localStorage.clear()
+      setIsAuth(false)
+      window.location.pathname="/login"
+    })
+  }
+
   return (
     <Router>
+       <nav>
+         <Link to="/">Home</Link>
+         <Link to="/createpost">Create Post</Link>
+         {!isAuth ? <Link to="/login">Login</Link> : <button type="" onClick={signUserOut}>Log out</button>}
+       </nav>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/login' element={<Login setIsAuth={setIsAuth}/>} />
         <Route path='/createpost' element={<CreatePost/>} />
       </Routes>
     </Router>
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+
+    <div className='cpContainer'>
+      <h1>
+        Create A Post
+      </h1>
+      <div className='inputGp'></div>
+      <div className='inputGp'></div>
+
+    </div>
+    </div>
   )
 }
